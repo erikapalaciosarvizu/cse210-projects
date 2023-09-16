c6216fe361a74053d0cd215e03a82eb161f77465
erikapalaciosarvizu <erika.palacios.arvizu@gmail.com>
2023-09-16 11:45:41 -0600

    requested data from the user

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?");
+        string firtsname = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {firtsname} {lastname}");
+
     }
 }
